90d397864637d0c8b9f7182d9e3d56d091926ea9
HellBz <stefan-koegler@freenet.de>
2026-09-13 00:29:16 +0200

    fix: simplify network check with sequential direct sockets

--- src/de/hellbz/forge/Utils/Net.java
+++ src/de/hellbz/forge/Utils/Net.java
@@ -3,7 +3,6 @@ package de.hellbz.forge.Utils;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.concurrent.*;
 
 import static de.hellbz.forge.Utils.Data.LogDebug;
 import static de.hellbz.forge.Utils.Data.LogInfo;
@@ -14,16 +13,16 @@ public class Net {
     public static boolean isConnected;
 
     static {
-
         if (!Config.isNetworkCheckEnabled()) {
             isConnected = false;
             LogInfo("Checking Internet... Skipped (network_check=false in " + Config.PROPERTIES_FILE + ").");
         } else {
             checkInternetConnection();
-            if (isConnected)
+            if (isConnected) {
                 LogInfo("Checking Internet... Connected to the Web.");
-            else
+            } else {
                 LogWarning("Checking Internet-Connection... Connection Failed.");
+            }
         }
     }
 
@@ -33,33 +32,22 @@ public class Net {
 
     public static void checkInternetConnection() {
         String[] hosts = {"1.1.1.1", "8.8.8.8"};
-        ExecutorService executor = Executors.newFixedThreadPool(hosts.length);
 
-        try {
-            for (String host : hosts) {
-                Future<Boolean> future = executor.submit(() -> canReachHost(host));
-                try {
-                    if (future.get(500, TimeUnit.MILLISECONDS)) {
-                        isConnected = true;
-                        return;
-                    }
-                } catch (InterruptedException | ExecutionException e) {
-                    LogDebug("Could not reach " + host + ": " + e.getMessage());
-                } catch (TimeoutException e) {
-                    LogDebug("Timeout while checking " + host);
-                    future.cancel(true);
-                }
+        for (String host : hosts) {
+            if (canReachHost(host, 443)) {
+                isConnected = true;
+                return;
             }
-        } finally {
-            executor.shutdownNow();
         }
     }
 
-    private static Boolean canReachHost(String host) {
+    private static boolean canReachHost(String host, int port) {
         try (Socket socket = new Socket()) {
-            socket.connect(new InetSocketAddress(host, 443), 750);
+            socket.connect(new InetSocketAddress(host, port), 1500);
+            LogDebug("Host " + host + " is reachable on port " + port);
             return true;
         } catch (IOException e) {
+            LogDebug("Could not reach " + host + " on port " + port + ": " + e.getMessage());
             return false;
         }
     }
